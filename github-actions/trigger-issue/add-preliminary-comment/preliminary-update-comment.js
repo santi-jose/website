1487efb713f5a75dd80ca4939e32175c3e4a3ab4
Jose Santiago <jlsantia21@gmail.com>
2026-06-08 21:29:25 -0700

    Update preliminary-update-comment.js
    Implement fix for double issue reading for pull request #8635

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -139,7 +139,7 @@ async function assignedToAnotherIssue() {
       owner: context.repo.owner,
       repo: context.repo.repo,
       assignee: assignee
-    })).data;
+    })).data.filter(issue => !issue.pull_request);
 
     const otherIssues = [];
 
