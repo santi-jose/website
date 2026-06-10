6123b182717a0a28b2cf11aa4ffefd71bb892c0a
Jose Santiago <jlsantia21@gmail.com>
2026-06-09 21:36:50 -0700

    Update preliminary-update-comment.js
    Adding final console.log's to isolate errors within logs for workflow.

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -112,8 +112,11 @@ async function main({ g, c }, { shouldPost, issueNum }) {
       console.log(' - change issue status to "New Issue Approval"');
     } else {
       // Otherwise, proceed with checks 
+      conosole.log("K");
       const comment = await createComment('preliminary-update.md', issueNum);
+      console.log("L");
       await postComment(issueNum, comment, github, context);
+      console.log("M");
     }
   } catch(error) {
     throw new Error(error);
